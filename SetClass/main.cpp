d97e219f0d476f3a0e8b039c7e69b251038695e8
Smak80 <smaklets@kpfu.ru>
2020-11-17 09:40:24 +0300

    Шаблонный класс множества.

--- SetClass/main.cpp
+++ SetClass/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "Set.h"
+#include "set.h"
 using namespace std;
 
 void main()
@@ -12,38 +12,52 @@ void main()
 	els[2] = 13;
 	els[3] = 45;
 	els[4] = 45;
-	Set s1(els);
-	cout << s1;
-	Set emptySet;
-	cout << emptySet;
+	set<int> s1(els);
+	//cout << s1;
+	s1.showSet();
+	set<int> emptySet;
+	//cout << emptySet;
+	emptySet.showSet();
 	els[0] = 1;
 	els[1] = 12;
 	els[2] = 113;
 	els[3] = 145;
 	els[4] = 45;
-	Set s5(els);
+	set<int> s5(els);
 	delete[] els;
-	cout << s5;
-	Set s2 = s1;
-	Set s3 = emptySet;
+	//cout << s5;
+	s5.showSet();
+	set<int> s2 = s1;
+	set<int> s3 = emptySet;
 	s3 = s2;
-	Set s4 = s1 + s5;
-	cout << s4;
-	Set s6 = s1 * s5;
-	cout << s6;
-	Set s7 = s1 / s5;
-	cout << s7;
-	Set s8 = s1 - s5;
-	cout << s8;
+	set<int> s4 = s1 + s5;
+	//cout << s4;
+	s4.showSet();
+	set<int> s6 = s1 * s5;
+	//cout << s6;
+	s6.showSet();
+	set<int> s7 = s1 / s5;
+	//cout << s7;
+	s7.showSet();
+	set<int> s8 = s1 - s5;
+	//cout << s8;
+	s8.showSet();
 	try {
-		cout << s8[1];
-		cout << s8[100];
-		cout << s8[0];
+		cout << s8[1] << " ";
+		cout << s8[100] << " ";
+		cout << s8[0] << " ";
 	}
 	catch(const char* exception)
 	{
 		cout << exception << endl;
 	}
-	
+	double* dels = new double[5];
+	dels[0] = 1.1;
+	dels[1] = 2.2;
+	dels[2] = 13.1;
+	dels[3] = 45.1;
+	dels[4] = 45.1;
+	set<double> ds1(dels);
+	ds1.showSet();
 	system("pause");
 }
---
+++ SetClass/map.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Set.h"
+/*class map :
+    public set
+{
+	struct pair
+	{
+		elem key;
+		double value;
+	};
+	//Список элементов (указатель на первый элемент)
+	pair* first_el = nullptr;
+	//Указатель на последний элемент списка
+	pair* last_el = nullptr;
+};
+
+*/
